789255168cfd421ff92b3c0365551559362ca332
sovilleda07 <sovilleda07@gmail.com>
2025-12-23 22:03:11 -0600

    Improve css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import './i18n.js';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
