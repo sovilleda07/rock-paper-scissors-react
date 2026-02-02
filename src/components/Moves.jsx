ab0f6b56c371b64e9d6d7281b10c1c9e6e116727
sovilleda07 <sovilleda07@gmail.com>
2026-02-01 18:16:50 -0600

    Fix images path

--- src/components/Moves.jsx
+++ src/components/Moves.jsx
@@ -6,13 +6,13 @@ export function Moves({ playerMove, computerMove }) {
   return (
     <div className="round-moves">
       <div className="player">
-        <img src={`/${playerMove}-emoji.png`} alt={playerMove} className='move-icon' />
+        <img src={`${playerMove}-emoji.png`} alt={playerMove} className='move-icon' />
         <span className="move-player">{t('move_player')}</span>
       </div>
       <span className="vs">{t('vs')}</span>
 
       <div className="player">
-        <img src={`/${computerMove}-emoji.png`} alt={computerMove} className='move-icon' />
+        <img src={`${computerMove}-emoji.png`} alt={computerMove} className='move-icon' />
         <span className="move-player">{t('move_computer')}</span>
       </div>
     </div>
